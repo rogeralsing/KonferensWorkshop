62a930f2cec761646e1f20df46bd5c662a06399d
rogeralsing <rogeralsing@gmail.com>
2015-09-12 14:29:35 +0200

    distributed log

---
+++ DistributedLog/WorkerNode/MyLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Event;
+
+namespace WorkerNode
+{
+    public class MyLogger : ReceiveActor
+    {
+        private void Log(LogEvent message)
+        {
+            Console.WriteLine("Log: {0}",message);
+        }
+
+        public MyLogger()
+        {
+            Receive<LogEvent>(m => Log(m));           
+            Receive<InitializeLogger>(m =>
+            {
+                Console.WriteLine("Starting MyLogger...");
+                Sender.Tell(new LoggerInitialized());
+            });
+        }
+    }
+}
